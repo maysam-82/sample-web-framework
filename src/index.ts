143be0efb99ede59b7b2cb4346981257d8675dd4
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-07 23:02:18 +1000

    add UserEdit and UserShow

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { UserForm } from './views/UserForm';
 import { User } from './models/User';
+import { UserEdit } from './views/UserEdit';
 const user = User.buildUser({ name: 'NAME', age: 9900 });
 const root = document.getElementById('root');
 if (root) {
-	const userForm = new UserForm(root, user);
-	userForm.render();
+	const userEdit = new UserEdit(root, user);
+	userEdit.render();
+	console.log(userEdit);
 } else {
 	throw new Error('Root element not found');
 }
